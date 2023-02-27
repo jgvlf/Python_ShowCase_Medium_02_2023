a8e4fde581f415af321b9980956cac03a9a5dcf0
João Gabriel Venceslau Lacerda Fernandes <51419682+joaogabrielvlf@users.noreply.github.com>
2023-02-27 19:26:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,20 +4,28 @@ green = "\033[1;32m"
 red = "\033[1;31m"
 cancel = "\033[m"
 
+
+def isfloat(num):
+    try:
+        float(num)
+        return True
+    except ValueError:
+        return False
+
 while True:
     nota_1 = str(input("Digite a primeira nota: "))
-    if(not nota_1.isnumeric()):
+    if(not isfloat(nota_1)):
         print("Valor da primeira nota não é numerica, porfavor digite novamente.")
     else:
-        nota_1 = int(nota_1)
+        nota_1 = float(nota_1)
         break
     
 while True:
     nota_2 = str(input("Digite a segunda nota: "))
-    if(not nota_2.isnumeric()):
+    if(not isfloat(nota_2)):
         print("Valor da segunda nota não é numerica, porfavor digite novamente.")
     else:
-        nota_2 = int(nota_2)
+        nota_2 = float(nota_2)
         break
 
 media = (nota_1+nota_2)/2
